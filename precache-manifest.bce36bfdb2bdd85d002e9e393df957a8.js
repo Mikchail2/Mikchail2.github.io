fcf7ac3def7be07c533f29d0364eaa3be5225bef
Mikchail2 <52310831+Mikchail2@users.noreply.github.com>
2019-10-14 22:43:38 +0300

    Add files via upload

---
+++ precache-manifest.bce36bfdb2bdd85d002e9e393df957a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d88eb8e0c1f9e287d4c40357bf302611",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08546db100312e19068d",
+    "url": "/static/css/main.ecef90de.chunk.css"
+  },
+  {
+    "revision": "f9503e2f6bfe157c33f5",
+    "url": "/static/js/2.65d35c40.chunk.js"
+  },
+  {
+    "revision": "08546db100312e19068d",
+    "url": "/static/js/main.6e85c952.chunk.js"
+  },
+  {
+    "revision": "706b3052af5a507f9a75",
+    "url": "/static/js/runtime-main.a3f51fd5.js"
+  }
+]);
